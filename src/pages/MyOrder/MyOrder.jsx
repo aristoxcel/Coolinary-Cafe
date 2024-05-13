93f245082cbf903b59b6a83e4f80f8b7e8ff859e
Rakib Hasan <rakibnkhadija@gmail.com>
2024-05-14 00:27:37 +0600

    fetch data from server side for user ordered food item and has been shown in ui

--- src/pages/MyOrder/MyOrder.jsx
+++ src/pages/MyOrder/MyOrder.jsx
@@ -1,9 +1,70 @@
+import { useContext, useEffect, useState } from "react"
 import Header from "../../components/Header"
+import { AuthContext } from "../../Providers/AuthProvider"
+import axios from "axios";
 
 
 function MyOrder() {
+  const {user} = useContext(AuthContext)
+  const [items, setItems] = useState([]);
+
+useEffect(() => {
+  const getData = async () => {
+    const { data } = await axios(
+      `${import.meta.env.VITE_API_URL}/order/${user?.email}`
+    );
+    setItems(data);
+  };
+      getData();
+    }, [user?.email])
+
+
+    // Function to format the date
+const formatDate = (purchaseTime) => {
+  const date = new Date(purchaseTime);
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-US', options);
+  const [day,  month, year] = formattedDate.split(' ');
+  return `${day} ${month} ${year}`;
+};
+
+
   return (
-    <div><Header image={'banner5.jpg'} text={'My Ordered Items'} ></Header></div>
+    <div>
+      <Header image={'banner5.jpg'} text={'My Ordered Items'} ></Header>
+      <div className="mt-52 lg:mt-[550px]  px-4 md:px-0 lg:px-4 container mx-auto">
+        <div className="space-y-5 px-3  lg:w-10/12 mx-auto rounded-2xl py-24 mb-3">
+          <h1 className="font-poppins text-2xl font-semibold text-base-400 font-poet">
+            Total   <span className="text-red-600">{items.length}</span> Food
+            items Ordered by  
+            <span className="uppercase text-teal-800"> {user?.displayName}</span>
+          </h1>
+          <hr />
+          <div>
+            <div className="overflow-x-auto space-y-8">
+              {
+                items.map(item=>(
+                  <>
+                  <div className="grid grid-cols-4 justify-center items-center border-2 border-teal-600 rounded-2xl p-4">
+                <div><img src={item.image} alt="" className="max-w-44 rounded-2xl"/></div>
+                <div className="col-span-2">
+                  <h2 className="font-poet text-gray-500">Total Cost: <span className="text-teal-800">{item.price} $</span></h2>
+                  <h1 className="font-poet text-gray-500">Product Name:<span className="text-teal-800">{item.food_name}</span></h1>
+                  <h3 className="font-poet text-gray-500">Date of Order: <span className="text-teal-800">{formatDate(item.purchase_time)}</span></h3>
+                  <h3 className="font-poet text-gray-500">Food Owner Name:<span className="text-teal-800">{item.cooker_name}</span></h3>
+                </div>
+                <div className="text-center space-y-4">
+                  <h1 className="font-poet text-gray-500 text-xl">Delete your Order?</h1>
+                  <button className="btn">Delete</button>
+                </div>
+              </div></>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
