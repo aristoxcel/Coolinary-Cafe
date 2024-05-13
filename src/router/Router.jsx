36a2b52b89481d9f4c3dc7a8e35a4e9e71429083
Rakib Hasan <rakibnkhadija@gmail.com>
2024-05-13 19:39:18 +0600

    purchased data passed to server side

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -13,6 +13,7 @@ import AddFood from "../pages/AddFood/AddFood";
 import MyOrder from "../pages/MyOrder/MyOrder";
 import Details from "../pages/Details";
 import UpdateForm from "../pages/MyAddFood/UpdateForm";
+import Purchase from "../pages/Purchase";
 
 
 
@@ -33,6 +34,8 @@ export const router =createBrowserRouter([
         {path:'/contact', element: <Contact></Contact>},
         {path:'/my-added-food', element: <MyAddFood></MyAddFood>},
         {path:'/update/:id', element: <UpdateForm></UpdateForm>,
+        loader:({params})=>fetch(`${import.meta.env.VITE_API_URL}/details/${params.id}`)},
+        {path:'/purchase/:id', element: <Purchase></Purchase>,
         loader:({params})=>fetch(`${import.meta.env.VITE_API_URL}/details/${params.id}`)},
         {path:'/add-food', element: <AddFood></AddFood>},
         {path:'/my-order', element: <MyOrder></MyOrder>},
